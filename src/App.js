67d6df0e8d52b6919ee06f32250a4410caa9bf3e
Yuliya <yulua03071992@gmail.com>
2023-06-13 16:18:58 +0300

    add projects and contacts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {NavBar} from './components/NavBar';
 import {Banner} from './components/Banner';
 import {Skills} from './components/Skills';
+import {Projects} from './components/Project';
+import {Contact} from './components/Contact';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
