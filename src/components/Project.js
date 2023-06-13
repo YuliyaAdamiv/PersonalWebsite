0e40b9d9ce35126f9bcca09307fa5d69d0c80e07
Yuliya <yulua03071992@gmail.com>
2023-06-13 17:00:52 +0300

    add project and edit tabs

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,11 @@ import {ProjectCard} from './ProjectCard';
 import projImg1 from '../assets/img/project-img1.png';
 import projImg2 from '../assets/img/project-img2.png';
 import projImg3 from '../assets/img/project-img3.png';
+import projImg4 from '../assets/img/project-img4.png';
+import projImg5 from '../assets/img/project-img5.png';
+import projImg6 from '../assets/img/project-img6.png';
+import projImg7 from '../assets/img/project-img7.png';
+import projImg8 from '../assets/img/project-img8.png';
 import colorSharp2 from '../assets/img/color-sharp2.png';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -27,17 +32,32 @@ export const Projects = () => {
     {
       title: 'Business Startup',
       description: 'Design & Development',
-      imgUrl: projImg1,
+      imgUrl: projImg4,
+    },
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg5,
+    },
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg6,
+    },
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg7,
+    },
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg8,
     },
     {
       title: 'Business Startup',
       description: 'Design & Development',
       imgUrl: projImg2,
-    },
-    {
-      title: 'Business Startup',
-      description: 'Design & Development',
-      imgUrl: projImg3,
     },
   ];
 
@@ -90,23 +110,19 @@ export const Projects = () => {
                           })}
                         </Row>
                       </Tab.Pane>
-                      <Tab.Pane eventKey="section">
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Cumque quam, quod neque provident velit, rem
-                          explicabo excepturi id illo molestiae blanditiis,
-                          eligendi dicta officiis asperiores delectus quasi
-                          inventore debitis quo.
-                        </p>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
                       </Tab.Pane>
                       <Tab.Pane eventKey="third">
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Cumque quam, quod neque provident velit, rem
-                          explicabo excepturi id illo molestiae blanditiis,
-                          eligendi dicta officiis asperiores delectus quasi
-                          inventore debitis quo.
-                        </p>
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
                       </Tab.Pane>
                     </Tab.Content>
                   </Tab.Container>
