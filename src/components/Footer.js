ff25d516c2a8179c5a34c489d9bb43a18d5e82fb
Yuliya <yulua03071992@gmail.com>
2023-06-14 16:25:15 +0300

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <p></p>
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/adamivyuliya/">
+                <img src={navIcon1} alt="Icon" />
+              </a>
+              <a href="https://github.com/YuliyaAdamiv">
+                <img src={navIcon2} alt="Icon" />
+              </a>
+              <a href="https://www.instagram.com/yuliya_1992_07/">
+                <img src={navIcon3} alt="Icon" />
+              </a>
+            </div>
+            <p>Copyright 2023. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
